9f7ee26cf5e3e86e86252ed5a2e2d628811d432c
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-11 09:57:03 -0700

    print lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print lower alphabet
+ * Return: always true(0)
+ *
+ *
+ */
+int main(void)
+
+{
+	int a = 97;
+
+	while (a <= 122)
+
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
