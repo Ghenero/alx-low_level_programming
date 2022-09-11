8d11d676856ee880ee8a2b2ce2edf52105c54f1c
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-11 11:48:48 -0700

    print both lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print mixed alphabets
+ * Return: always (0)
+ *
+ *
+ *
+ */
+int main(void)
+
+{
+	int a = 97;
+	int b = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
