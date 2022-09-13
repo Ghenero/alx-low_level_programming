c7f468c8f0438870333bf455a9f294b70891521b
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-13 09:03:07 -0700

    print single combine number without repition not exceeding 89

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - print two digit combination without repeatition in ancending order
+ *
+ * Return: Always true(0)
+ *
+ *
+ */
+
+int main(void)
+
+{
+
+	int a = 48;
+	int b = 48;
+
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 58)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
