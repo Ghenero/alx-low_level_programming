b6351843c267152dfddebda612043d9f45ebe31e
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-14 08:05:53 -0700

    prints all possible combinations of two two-digit numbers using comma and spaces between them

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,28 @@
 int main(void)
 
 {
-	unsigned char abc = 0;
-	unsigned char xyz = 1;
+	int a = 0;
+	int b = 1;
 
-	while (abc < 99)
+	while (a < 99)
 	{
-		while (xyz < 100)
+		while (b < 100)
 		{
-			putchar((abc / 10) + 48);
-			putchar((abc % 10) + 48);
+			putchar((a / 10) + 48);
+			putchar((a % 10) + 48);
 			putchar(' ');
-			putchar((xyz / 10) + 48);
-			putchar((xyz % 10) + 48);
-			if (abc == 98 && xyz == 99)
+			putchar((b / 10) + 48);
+			putchar((b % 10) + 48);
+			if (a == 98 && b == 99)
 			{
 				break;
 			}
 			putchar(',');
 			putchar(' ');
-			xyz++;
+			b++;
 		}
-		abc++;
-		xyz = abc + 1;
+		a++;
+		b = a + 1;
 	}
 	putchar('\n');
 	return (0);
