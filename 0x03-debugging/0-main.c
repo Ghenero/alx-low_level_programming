dede8823237e4efbcb380a379d82b719c3f153e3
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-15 19:39:45 -0700

    A program that run test function for an integer whether is positive, negative ive and correct output in case of 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - A program that print if integer is poitive , zero or negative
+ *
+ * Return: Always true(0)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i - 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is a a directory about learning how to debug your codes and how to check your code for errors 
