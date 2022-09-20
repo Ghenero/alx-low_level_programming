f5cd0be96c953da94d64f3cbe58290849bf4d799
Ghenero <egbetamahoghenero@yahoo.com>
2022-09-19 21:15:46 -0700

    a program tha write a  function that swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,17 @@
 include "main.h"
 
 /**
- * swap int - swap two integers value and ouput the result
+ * swap_int - swap two integers value and ouput the result
  * @a: first integer
  * @b: second integer
- * Return: Always true(0)
+ * Return: 0
  */
 
 void swap_int(int *a, int *b)
-
 {
 	int change = *a;
 
 	*a = *b;
-	*b = *change;
+	*b = change;
+	return (0);
 }
