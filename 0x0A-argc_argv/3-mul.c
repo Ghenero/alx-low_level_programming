cc63496ac08bf0fca3e063dde55c2af192a26dab
Ghenero <egbetamahoghenero@yahoo.com>
2022-10-05 07:16:22 -0700

    A program that two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the result of the two number multiplied
+ * @argc: arguments input tothe function
+ * @argv: argument vectors that stores the strings in an array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int number_1, number_2, muitiply;
+
+	if (arg != 3)
+		printf("Error\n");
+	else
+	{
+		number_1 = atoi(argv[1]);
+		number_2 = atoi(argv[2]);
+		muitiply = number_1 * number_2;
+		printf("%d\n", muitiply);
+	}
+	return (0);
+}
+
